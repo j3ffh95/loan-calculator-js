8ebeb546058adb5ffa8ec95b3e859aba4897e8a3
j3ffh95 <j3ffh95@gmail.com>
2020-07-12 12:49:40 -0400

    Added the calculateResults function and set the UI variables

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+// Listen for submit
+document
+  .getElementById("loan-form")
+  .addEventListener("submit", calculateResults);
 
+// Calculate Results function
+function calculateResults(e) {
+  // UI Variables
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+
+  e.preventDefault();
+}
